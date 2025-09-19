1bdcbb5eeadf134cbd765a6d14794d95467c785a
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2025-09-19 17:07:26 +0400

    Add Init Project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected title = 'TKLifeConnect';
